80faccc80144075694f3f86170a7ddbfca8ab9b9
jzen <jzen.soft@gmail.com>
2021-02-16 14:53:06 +0700

    edit peer id

--- src/App.js
+++ src/App.js
@@ -1,122 +1,261 @@
-import React, { useState, useEffect } from "react";
-import Peer from "peerjs";
+import React, { useState, useEffect, useRef } from 'react';
+import Peer from 'peerjs';
+import {
+  Container,
+  Row,
+  Col,
+  Alert,
+  Card,
+  CardBody,
+  Button,
+  Badge,
+  Tooltip,
+} from "shards-react";
 
-var peer = new Peer({
-  host: "https://mypeerjs611463022.herokuapp.com/",
-  port: 443,
-  secure: true,
-});
-var conn = peer.connect();
+var peer = new Peer();
 
 const App = () => {
-  const [myID, setMyID] = useState("");
-  const [friendID, setFriendID] = useState("");
-  const [connected, setConnected] = useState(false);
-  const [files, setFiles] = useState([]);
+
+  const [myID, setMyID] = useState('')
+  const [friendID, setFriendID] = useState('')
+  const [mystream, setsMytream] = useState()
+
+  const [stateConnect, setStateConnect] = useState(false)
+  const [stateButton, setSateButton] = useState(false)
+  let [stateTooltip, setStateTooltip] = useState(false)
+  let [stateCopy, setStateCopy] = useState(false)
+  let [stateDisConnect, setSateDisConnect] = useState(false)
+
+  const myVideo = useRef();
+  const friendVideo = useRef();
+
+  // Set Title
+  document.title = myID
 
   useEffect(() => {
-    peer.on("open", (id) => {
+
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
+      setsMytream(stream);
+      if (myVideo.current) {
+        myVideo.current.srcObject = stream;
+      }
+    })
+
+    peer.on('open', id => {
       setMyID(id);
     });
 
-    peer.on("connection", (connection) => {
-      conn = connection;
-      setConnected(true);
+    peer.on('connection', conn => {
+      setStateConnect(true)
 
-      connection.on("open", () => {});
+      // Receive Data
+      conn.on('data', data => {
+        setFriendID(data)
+      })
 
-      connection.on("data", (data) => {
-        onReceiveData(data);
-      });
     });
+
+    peer.on("disconnect", () => {
+      setStateConnect(false)
+    });
+
+    // Answer
+    peer.on('call', call => {
+      navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+        .then((stream) => {
+          call.answer(stream)
+          call.on('stream', stream => {
+            friendVideo.current.srcObject = stream
+          })
+        })
+        .catch((err) => {
+          console.error('Failed to get local stream', err);
+        });
+    })
+
   }, []);
 
-  const Connect = (friendD) => {
-    conn = peer.connect(friendID);
 
-    conn.on("open", () => {
-      setConnected(true);
-    });
+  function startConnection() {
 
-    conn.on("data", (data) => {
-      onReceiveData(data);
-    });
-  };
+    const conn = peer.connect(friendID);
+    setStateConnect(true)
 
-  const SendFile = (e) => {
-    const file = e.target.files[0];
+    // Send Friend ID
+    conn.on('open', () => {
+      conn.send(friendID)
+    })
 
-    if (file.size <= 5242880) {
-      const blob = new Blob(e.target.files, { type: file.type });
+    peer.on('error', err => {
+      if (err.type === 'peer-unavailable')
+        alert("The id or peer you're trying to connect to does not exist.")
+    })
 
-      conn.send({
-        file: blob,
-        filename: file.name,
-        filetype: file.type,
-      });
-    } else {
-      alert("The file is larger than 5 MB.");
-    }
-  };
+    // Call
+    let call = peer.call(friendID, mystream)
+    call.on('stream', stream => {
+      friendVideo.current.srcObject = stream
+    })
 
-  const onReceiveData = (data) => {
-    const blob = new Blob([data.file], { type: data.filetype });
-    const url = URL.createObjectURL(blob);
+  }
 
-    addFile({ name: data.filename, url: url });
-  };
+  // DisConnect Peer and Refresh Page
+  const DisConnect = () => {
+    peer.destroy()
+    window.location.reload(false)
+  }
 
-  const addFile = (file) => {
-    const data = { name: file.name, url: file.url };
-    setFiles((files) => [...files, data]);
-  };
+  // My Video Conponent
+  let MyVideo;
+  if (mystream) {
+    MyVideo = (
+      <video playsInline muted ref={myVideo} autoPlay style={{ maxWidth: "100%" }} />
+    );
+  }
+
+  // Friend Video Conponent
+  let FriendVideo;
+  if (friendVideo) {
+    FriendVideo = (
+      <video playsInline muted ref={friendVideo} autoPlay style={{ maxWidth: "100%" }} />
+    );
+  }
+
+  const toggleTooltip = () => {
+    setStateTooltip(stateTooltip ? false : true)
+  }
+
+  const copyMyID = () => {
+    setStateCopy(stateCopy ? false : true)
+    navigator.clipboard.writeText(myID)
+  }
+
+  const togleStateDisConnect = () => {
+    setSateDisConnect(stateDisConnect ? false : true)
+  }
+
+  const handleChange = (e) => {
+
+    setFriendID(e.target.value)
+
+    if (e.target.value.length > 10)
+      setSateButton(true)
+    else
+      setSateButton(false)
+
+  }
 
   return (
-    <div className="App">
-      <h3>My ID : {myID}</h3>
+    <>
+      <Container>
+        <Row className="mt-2">
+          <Col md="6">
+            <Alert style={{ marginBottom: "0" }}>
+              My Video | Peer is {myID}
+            </Alert>
+            {MyVideo}
+          </Col>
 
-      {connected ? (
-        <h4 style={{ color: "green" }}>Connected</h4>
-      ) : (
-        <h4 style={{ color: "red" }}>Not Connect</h4>
-      )}
+          <Col md="6" style={{ padding: stateConnect ? ('') : ("0"), border: stateConnect ? ('') : ("1px solid #ccc") }}>
+            <Alert theme="secondary" style={{ marginBottom: "0" }}>
+              Friend Video {friendID ? (`| Peer is ${friendID}`) : ('')}
+            </Alert>
+            {
+              stateConnect ? FriendVideo : (<h3 className="text-center" style={{ marginTop: "20%", color: "#ccc" }}>No video yet</h3>)
+            }
+          </Col>
+        </Row>
 
-      {connected ? (
-        <div>
-          <input
-            type="file"
-            name="file"
-            id="file"
-            onChange={(e) => SendFile(e)}
-          />
-        </div>
-      ) : (
-        <div>
-          <input
-            type="text"
-            value={friendID}
-            onChange={(e) => setFriendID(e.target.value)}
-            placeholder={myID}
-          />
-          <button type="submit" onClick={() => Connect(friendID)}>
-            Connect
-          </button>
-        </div>
-      )}
 
-      <div>
-        <ul>
-          {files.map((file, index) => (
-            <li key={index}>
-              <a href={file.url} download={file.name}>
-                {file.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+        <Card className="mt-5 mb-2">
+          <CardBody>
+            {
+              stateConnect ?
+                (
+                  <Row>
+
+                    <Col md="3">
+
+                      {
+                        stateDisConnect ? (
+                          <Badge theme="danger" onMouseLeave={togleStateDisConnect} onClick={DisConnect} style={{ cursor: "pointer" }}>
+                            <h4 style={{ marginBottom: "0", padding: "10px", color: "#FFF" }}>
+                              DisConnect
+                            </h4>
+                          </Badge>
+                        ) : (
+                            <Badge theme="success" onMouseEnter={togleStateDisConnect} >
+                              <h4 style={{ marginBottom: "0", padding: "10px", color: "#FFF" }}>
+                                Connected Peer ID
+                            </h4>
+                            </Badge>
+                          )
+                      }
+
+
+
+                    </Col>
+
+                    <Col md="9">
+                      <h4 style={{ margin: "0", padding: "10px" }}>
+                        {friendID}
+                      </h4>
+                    </Col>
+
+                  </Row>
+                )
+                :
+                (
+                  <Row>
+                    <Row>
+                      <Col>
+                        <h4>
+
+                          <Badge className="mr-3" theme="info">My ID</Badge>
+                          <label style={{ cursor: "pointer" }} id="myId" onClick={copyMyID}>
+                            {myID}
+                          </label>
+
+                        </h4>
+                        <Tooltip
+                          open={stateTooltip}
+                          toggle={toggleTooltip}
+                          target="#myId"
+                        >
+                          {
+                            stateCopy ? ("Copied") : ("Clicke to Coppy")
+                          }
+                        </Tooltip>
+                      </Col>
+                    </Row>
+
+                    <Row>
+                      <Col md="10">
+                        <div className="form-floating mb-3">
+                          <input type="text" className="form-control" id="friendID" placeholder="Enter Friend ID" value={friendID} onChange={e => handleChange(e)} />
+                          <label htmlFor="friendID">Enter Friend ID</label>
+                        </div>
+                      </Col>
+
+                      <Col md="2">
+                        {
+                          stateButton ? (
+                            <Button size="lg" pill theme="success" onClick={startConnection} >Connect</Button>
+                          ) : (
+                              <Button size="lg" pill theme="success" disabled >Connect</Button>
+                            )
+                        }
+                      </Col>
+                    </Row>
+                  </Row>
+                )
+            }
+
+          </CardBody>
+        </Card>
+      </Container>
+    </>
   );
-};
+}
 
 export default App;
